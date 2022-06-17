d7254d5b950af49e857743eeb79097001d801dd4
Ying Liu <yingliu4203@gmail.com>
2022-06-16 20:07:27 -0700

    implement login and register UI

--- screens/LoginScreen/index.jsx
+++ screens/LoginScreen/index.jsx
@@ -1,12 +1,66 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { StyleSheet, View, KeyboardAvoidingView } from 'react-native';
+import React, { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
 
-export default function LoginScreen() {
+import { Button, Input, Image } from '@rneui/base';
+
+export default function LoginScreen({ navigation }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function login() {
+    console.log('user login');
+  }
+
   return (
-    <View>
-      <Text>Login Screen</Text>
-    </View>
+    <KeyboardAvoidingView style={styles.container}>
+      <StatusBar style="light" />
+      <Image
+        source={{
+          uri: 'https://blog.mozilla.org/internetcitizen/files/2018/08/signal-logo.png',
+        }}
+        style={styles.image}
+      />
+      <View style={styles.inputContainer}>
+        <Input
+          placeholder="Email"
+          hjk
+          autoFocus
+          type="email"
+          onChangeText={setEmail}
+        />
+        <Input
+          placeholder="Password"
+          secureTextEntry
+          type="password"
+          onChangeText={setPassword}
+          onSubmitEditing={login}
+        />
+      </View>
+      <Button title="Login" containerStyle={styles.button} onPress={login} />
+      <Button
+        title="Register"
+        containerStyle={styles.button}
+        type="outline"
+        onPress={() => navigation.navigate('Register')}
+      />
+    </KeyboardAvoidingView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'cetner',
+    padding: 10,
+  },
+  image: { width: 200, height: 200 },
+  inputContainer: {
+    width: 300,
+  },
+  button: {
+    width: 200,
+    marginTop: 10,
+  },
+});
--- screens/RegisterScreen/index.jsx
+++ screens/RegisterScreen/index.jsx
@@ -1,12 +1,71 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { StyleSheet, View, KeyboardAvoidingView } from 'react-native';
+import React, { useState, useLayoutEffect } from 'react';
+import { StatusBar } from 'expo-status-bar';
 
-export default function RegisterScreen() {
+import { Button, Input, Text } from '@rneui/base';
+
+export default function RegisterScreen({ navigation }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerBackTitle: 'Back to Login', // iOS only
+    });
+  }, [navigation]);
+
+  function register() {
+    console.log('register called');
+  }
+
   return (
-    <View>
-      <Text>Register Screen</Text>
-    </View>
+    <KeyboardAvoidingView style={styles.container}>
+      <StatusBar style="light" />
+      <Text h3 style={{ marginBottom: 50 }}>
+        Create a Chat Account
+      </Text>
+      <View style={styles.inputContainer}>
+        <Input
+          placeholder="Full Name"
+          autofocus
+          type="text"
+          onChangeText={setName}
+        />
+        <Input placeholder="Email" type="email" onChangeText={setEmail} />
+        <Input
+          placeholder="Passord"
+          type="password"
+          secureTextEntry
+          onChangeText={setPassword}
+        />
+        <Input
+          placeholder="Profile Pic UR"
+          type="text"
+          onChangeText={setImageUrl}
+          onSubmitEditing={register}
+        />
+      </View>
+
+      <Button
+        style={styles.button}
+        raised
+        title={'Register'}
+        onPress={register}
+      />
+    </KeyboardAvoidingView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+    backgroundColor: 'white',
+  },
+  inputContainer: { width: 300 },
+  button: { width: 200, marginTop: 10 },
+});
