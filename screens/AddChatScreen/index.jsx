7e49a101806dd05f6799f500c20d7dbf9744c683
Ying Liu <yingliu4203@gmail.com>
2022-06-21 19:14:14 -0700

    add new chat ui

--- screens/AddChatScreen/index.jsx
+++ screens/AddChatScreen/index.jsx
@@ -1,7 +1,11 @@
-import React, { useLayoutEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useLayoutEffect, useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import { Button, Input } from '@rneui/base';
 
 export default function AddChatScreen({ navigation }) {
+  const [chatName, setChatName] = useState('');
+
   useLayoutEffect(() => {
     navigation.setOptions({
       title: 'Add a New Chat',
@@ -9,11 +13,29 @@ export default function AddChatScreen({ navigation }) {
     });
   }, [navigation]);
 
+  function createChat() {
+    console.log('create chat');
+  }
+
   return (
-    <View>
-      <Text>Add Chat Room</Text>
+    <View style={styles.container}>
+      <Input
+        placeholder="Enter a chat name"
+        onChangeText={setChatName}
+        onSubmitEditing={createChat}
+      />
+      <Button
+        title="Create a New Chat"
+        onPress={createChat}
+        disabled={!chatName}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    padding: 30,
+    height: '100%',
+  },
+});
