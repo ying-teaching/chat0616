022e1509f628c6e0843c5b227064fb6db7e314f8
Ying Liu <yingliu4203@gmail.com>
2022-06-21 19:05:32 -0700

    change titles

--- screens/AddChatScreen/index.jsx
+++ screens/AddChatScreen/index.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
 export default function AddChatScreen({ navigation }) {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: 'Add a New Chat',
+      headerBackTitle: 'Chats',
+    });
+  }, [navigation]);
+
   return (
     <View>
       <Text>Add Chat Room</Text>
